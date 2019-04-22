4e96b48e72542f94a46c005755ae452ab0988536
Aliya <Aliya Salimova>
2019-04-22 14:16:41 +0500

    useState and useEffect hooks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const todo = props => {
     const [todoName, setTodoName] = useState('');
+    const [todoList, setTodoList] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://my-hooks-7b65e.firebaseio.com/todos.json')
+        .then(result => {
+            console.log(result);
+            const todoData = result.data;
+            const todos = [];
+            for (const key in todoData) {
+                todos.push(todoData[key].name);
+            }
+            setTodoList(todos);
+        });
+        return () => {
+            console.log('Cleanup');
+        }
+    }, []);
+
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value);
     };
 
+    const todoAddHandler = () => {
+        setTodoList(todoList.concat(todoName));
+        axios.post('https://my-hooks-7b65e.firebaseio.com/todos.json', {name: todoName})
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+
     return <React.Fragment>
         <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={todoName}/>
-        <button type="button">Add</button>
-
+        <button type="button" onClick={todoAddHandler}>Add</button>
+        <ul>
+            {todoList.map(todo => <li key={todo}>{todo}</li>)}
+        </ul>
     </React.Fragment>
 };
 
