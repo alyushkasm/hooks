1b44aa72eb5376a4d1b39068304bb9198bdd5fc3
Aliya <Aliya Salimova>
2019-05-04 17:57:58 +0500

    useContext and useReducer hooks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 
 const todo = props => {
     const [todoName, setTodoName] = useState('');
-    const [todoList, setTodoList] = useState([]);
+    const [submittedTodo, setSubmittedTodo] = useState(null);
+    //const [todoList, setTodoList] = useState([]);
 
     useEffect(() => {
         axios.get('https://my-hooks-7b65e.firebaseio.com/todos.json')
@@ -12,23 +13,45 @@ const todo = props => {
             const todoData = result.data;
             const todos = [];
             for (const key in todoData) {
-                todos.push(todoData[key].name);
+                todos.push({id: key, name: todoData[key].name});
             }
-            setTodoList(todos);
+            dispatch({type: 'SET', payload: todos});
         });
         return () => {
             console.log('Cleanup');
         }
     }, []);
 
+    useEffect(() => {
+        if (submittedTodo) {
+            dispatch({type: 'ADD', payload: submittedTodo});
+        }  
+    }, [submittedTodo]);
+
+    const todoListReducer = (state, action) => {
+        switch(action.type) {
+            case 'ADD':
+                return state.concat(action.payload);
+            case 'SET':
+                return action.payload;
+            case 'REMOVE':
+                return state.filter((todo) => todo.id !== action.payload);
+            default:
+                return state;
+        }
+    }
+
+    const [todoList, dispatch] = useReducer(todoListReducer, []);
+
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value);
     };
 
     const todoAddHandler = () => {
-        setTodoList(todoList.concat(todoName));
         axios.post('https://my-hooks-7b65e.firebaseio.com/todos.json', {name: todoName})
         .then(res => {
+            const todoItem = { id: res.data.name, name: todoName };
+            setSubmittedTodo(todoItem);
             console.log(res);
         })
         .catch(err => {
@@ -40,7 +63,7 @@ const todo = props => {
         <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={todoName}/>
         <button type="button" onClick={todoAddHandler}>Add</button>
         <ul>
-            {todoList.map(todo => <li key={todo}>{todo}</li>)}
+            {todoList.map(todo => <li key={todo.id}>{todo.name}</li>)}
         </ul>
     </React.Fragment>
 };
