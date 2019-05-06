36c7c6836fb4b4a94a5ab8f4c716770ee9237101
Aliya <Aliya Salimova>
2019-05-06 16:21:48 +0500

    useReducer and useRef hooks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer, useRef } from 'react';
 import axios from 'axios';
 
 const todo = props => {
-    const [todoName, setTodoName] = useState('');
-    const [submittedTodo, setSubmittedTodo] = useState(null);
+    //const [todoName, setTodoName] = useState('');
+    //const [submittedTodo, setSubmittedTodo] = useState(null);
     //const [todoList, setTodoList] = useState([]);
 
     useEffect(() => {
@@ -22,11 +22,13 @@ const todo = props => {
         }
     }, []);
 
-    useEffect(() => {
-        if (submittedTodo) {
-            dispatch({type: 'ADD', payload: submittedTodo});
-        }  
-    }, [submittedTodo]);
+    // useEffect(() => {
+    //     if (submittedTodo) {
+    //         dispatch({type: 'ADD', payload: submittedTodo});
+    //     }  
+    // }, [submittedTodo]);
+
+    const todoInputRef = useRef();
 
     const todoListReducer = (state, action) => {
         switch(action.type) {
@@ -43,15 +45,16 @@ const todo = props => {
 
     const [todoList, dispatch] = useReducer(todoListReducer, []);
 
-    const inputChangeHandler = (event) => {
-        setTodoName(event.target.value);
-    };
+    // const inputChangeHandler = (event) => {
+    //     setTodoName(event.target.value);
+    // };
 
     const todoAddHandler = () => {
+        const todoName = todoInputRef.current.value;
         axios.post('https://my-hooks-7b65e.firebaseio.com/todos.json', {name: todoName})
         .then(res => {
             const todoItem = { id: res.data.name, name: todoName };
-            setSubmittedTodo(todoItem);
+            dispatch({type: 'ADD', payload: todoItem});
             console.log(res);
         })
         .catch(err => {
@@ -59,11 +62,20 @@ const todo = props => {
         });
     }
 
+    const todoRemoveHandler = todoId => {
+        axios.delete(`https://my-hooks-7b65e.firebaseio.com/todos/${todoId}.json`)
+        .then(res => {
+            dispatch({type: 'REMOVE', payload: todoId});
+        })
+        .catch(err => console.log(err));
+        
+    }
+
     return <React.Fragment>
-        <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={todoName}/>
+        <input type="text" placeholder="Todo" ref={todoInputRef} />
         <button type="button" onClick={todoAddHandler}>Add</button>
         <ul>
-            {todoList.map(todo => <li key={todo.id}>{todo.name}</li>)}
+            {todoList.map(todo => <li key={todo.id} onClick={todoRemoveHandler.bind(this, todo.id)}>{todo.name}</li>)}
         </ul>
     </React.Fragment>
 };
