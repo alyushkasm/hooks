bae2201aff44cb1478cb22ab8add2d5efc2949d7
Aliya <Aliya Salimova>
2019-05-27 17:57:32 +0500

    optimizing and preparing

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useReducer, useRef } from 'react';
+import React, { useState, useEffect, useReducer, useRef } from 'react';
+import List from './List';
 import axios from 'axios';
 
 const todo = props => {
+    const [inputIsValid, setInputIsValid] = useState(false);
     //const [todoName, setTodoName] = useState('');
     //const [submittedTodo, setSubmittedTodo] = useState(null);
     //const [todoList, setTodoList] = useState([]);
@@ -21,6 +23,14 @@ const todo = props => {
             console.log('Cleanup');
         }
     }, []);
+
+    const inputValidationHandler = event => {
+        if (event.target.value.trim() === '') {
+            setInputIsValid(false);
+        } else {
+            setInputIsValid(true);
+        }
+    }
 
     // useEffect(() => {
     //     if (submittedTodo) {
@@ -72,11 +82,14 @@ const todo = props => {
     }
 
     return <React.Fragment>
-        <input type="text" placeholder="Todo" ref={todoInputRef} />
+        <input 
+            type="text" 
+            placeholder="Todo" 
+            ref={todoInputRef} 
+            onChange={inputValidationHandler}
+            style={{backgroundColor: inputIsValid ? 'transparent' : 'red'}} />
         <button type="button" onClick={todoAddHandler}>Add</button>
-        <ul>
-            {todoList.map(todo => <li key={todo.id} onClick={todoRemoveHandler.bind(this, todo.id)}>{todo.name}</li>)}
-        </ul>
+        <List items={todoList} onClick={todoRemoveHandler}/>
     </React.Fragment>
 };
 
